cb70337836ca910fc63de773c408d43b7516a081
Brayzen <brian.ray.biz@gmail.com>
2015-09-03 14:24:59 -0700

    638_035 iterations.....a final fibonacci number of 132_522 digits long

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,18 +5,21 @@ class Fibonacci
   def run
     while @running do
       @fibonacci << @fibonacci.last(2).inject(:+)
+      @fibonacci.shift
+      @count += 1
     end
   end
 
   def start(seconds)
+    @count = 3
     @fibonacci = [1, 1]
     continue seconds
   end
 
   def stop
     @running = false
-    # puts @fibonacci
-    puts @fibonacci.count
+    puts @fibonacci.last.to_s.length
+    puts @count
   end
 
   def continue(seconds)
