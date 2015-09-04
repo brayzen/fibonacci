69b827c1970bbbedf4ef53f07a53b8f698083e52
Brayzen <brian.ray.biz@gmail.com>
2015-09-04 11:08:42 -0700

    function not yet working

--- fibonacci.rb
+++ fibonacci.rb
@@ -2,19 +2,19 @@ require 'timeout'
 
 class Fibonacci
 
-  def run
-    while @running do
-      @fibonacci << @fibonacci.last(2).inject(:+)
-      @fibonacci.shift
-      @count += 1
-    end
-  end
-
   def start(seconds)
     @count = 2
     @fibonacci = [0, 1]
     continue seconds
   end
+
+  def run
+    while @running do
+      @fibonacci.push(@fibonacci.last(2).inject(:+)).shift
+      @count += 1
+    end
+  end
+
 
   def stop
     @running = false
@@ -29,6 +29,14 @@ class Fibonacci
     rescue Timeout::Error
       stop
     end
+  end
+
+  def find_fib(n)
+    raise "Can't have a number less than or equal to 0" if n <= 0
+    return 1 if n == 2
+    return 0 if n == 1
+    @fibonacci = [0, 1]
+    (n - 1).times {@fibonacci.push(@fibonacci.last(2).inject(:+)).shift}
   end
 end
 
--- fibonacci_test.rb
+++ fibonacci_test.rb
@@ -1,4 +1,17 @@
 require './fibonacci'
 
 fibtest = Fibonacci.new
-fibtest.start 10
+fibtest.start 1
+
+
+p "1: #{fibtest.find_fib(1)}"
+p "2: #{fibtest.find_fib(2)}"
+p "3: #{fibtest.find_fib(3)}"
+p "4: #{fibtest.find_fib(4)}"
+p "5: #{fibtest.find_fib(5)}"
+p "6: #{fibtest.find_fib(6)}"
+p "7: #{fibtest.find_fib(7)}"
+p "10: #{fibtest.find_fib(10)}"
+# p fibtest.find_fib(0)
+# p fibtest.find_fib(-1)
+
